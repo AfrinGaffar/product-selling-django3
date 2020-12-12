a7c65a32d5ab496d6719bb2e50869f337177e1b1
Afrin Begum <72100064+AfrinGaffar@users.noreply.github.com>
2020-12-12 19:35:42 +0800

    Add files via upload

---
+++ app_payment/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from app_payment.models import BillingAddress
+
+class BillingForm(forms.ModelForm):
+    class Meta:
+        model = BillingAddress
+        fields = ['address','zipcode','city','country']
